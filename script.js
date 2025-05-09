e2efb960ee593ed606a77c4157d6ce29a09533d5
VISHNUPRASAD K G <110888065+VISHNUPRASAD-K-G@users.noreply.github.com>
2025-05-10 01:39:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,20 +73,15 @@ function weatherDisply(){
               
             crnt_time = getTimeFromOffsetSeconds(items.timezone);
             const time = crnt_time.split(' ');
-            // tm = time[0].split(':')[0];
-            const bg_img_container = document.getElementById('bg_img_container');
-            // if((time[1].toLowerCase() == 'pm' && (tm >= 7 && tm< 12)) || (time[1].toLowerCase() == 'am' && ((tm <= 6 || tm == 12)))){
-            bg_img_container.style.backgroundImage = "url('https://wallpapers-clan.com/wp-content/uploads/2024/03/starfall-night-sky-mountains-aesthetic-gif-preview-desktop-wallpaper.gif')";
-            console.log("darked mode on.");
-            //     bg_img_container.style.backgroundSize = 'cover';
-            //     bg_img_container.style.width='100%';
-            //     bg_img_container.style.height='100vh';
-            // }
-            // else{
-            //     bg_img_container.style.backgroundImage = 'url(https://cdn.pixabay.com/animation/2023/03/11/17/29/17-29-27-410_512.gif)';
-            // }
+            tm = time[0].split(':')[0];
+            if((time[1].toLowerCase() == 'pm' && (tm >= 7 && tm< 12)) || (time[1].toLowerCase() == 'am' && ((tm <= 6 || tm == 12)))){
+                bg_img_container.style.backgroundImage = "url('https://wallpapers-clan.com/wp-content/uploads/2024/03/starfall-night-sky-mountains-aesthetic-gif-preview-desktop-wallpaper.gif')";
+            }
+            else{
+                bg_img_container.style.backgroundImage = "url('https://cdn.pixabay.com/animation/2023/03/11/17/29/17-29-27-410_512.gif')";
+            }
             // weather_containers.style.display = 'block';
-
+            console.log("hai Every one");
             // bg_img(wea_desc, crnt_temp, weather_name,crnt_time);
 
             item_container.innerHTML = `
